473935baf2aef56d520d396361cadb40c41165ac
parker <jonparkerbrooks@gmail.com>
2022-05-05 17:49:26 -0500

    dfs hotfix

--- src/algos.cpp
+++ src/algos.cpp
@@ -33,36 +33,6 @@ vector<string> BFSdst(Graph g, string src, string dst) {
   return {};
 }
 
-vector<string> BFSdst(Graph g, string src, string dst) {
-
-  queue<pair<string, vector<string>>> nodes; // node queue
-  set<string> visited;// set of visited nodes
-  vector<string> nodePath;
-  string node;
-
-  nodes.push({src, {src}});
-
-  while(!nodes.empty()) {
-    
-    node = nodes.front().first;
-    nodePath = nodes.front().second;
-    nodes.pop();
-    
-    if(node == dst)
-      return nodePath;
- 
-    for(string connection : g.getNodes(node)) {
-      if(visited.count(connection) == 0) {
-        visited.insert(connection);
-        nodePath.push_back(connection);
-        nodes.push({connection, nodePath});
-      }
-    }
-  }
-
-  return {};
-}
-
 vector<string> BFSall(Graph g, string src) {
 
   queue<pair<string, vector<string>>> nodes; // node queue
